83734839156363b1bfcb5736f5fdff5f61784391
Opilane TTHK <opilane@tthk.ee>
2025-11-13 10:32:19 +0200

    Calculator

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float result = 0;
+
+            Console.WriteLine("Sisesta esimene number");
+           float firstNumber = float.Parse(Console.ReadLine());
+            Console.WriteLine("Sisesta teine number");
+            float secondnumber = float.Parse(Console.ReadLine());
+            Console.WriteLine("Vali tehe +,-,*,/");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "+":
+                    Console.WriteLine($"tulemus: {firstNumber + secondnumber}");
+                    break;
+                case "-":
+                    Console.WriteLine($"tulemus: {firstNumber - secondnumber}");
+                    break;
+                case "*":
+                    Console.WriteLine($"tulemus: {firstNumber * secondnumber}");
+                    break;
+                case "/":
+                    Console.WriteLine($"tulemus: {firstNumber / secondnumber}");
+                    break;
+                    if (secondnumber == 0)
+                    {
+                        Console.WriteLine("ne");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Tulemus: {firstNumber / secondnumber}");
+                    }
+                    break;
+                    default:
+                    break;
+
+                        
+                    
+            }
+
+
+
+        }
+    }
+}
